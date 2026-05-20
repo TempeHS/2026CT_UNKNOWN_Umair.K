f21344d74e97da3f01b1af906a009d9295d4de7b
Umair K <umair.khan7@education.nsw.gov.au>
2026-05-20 10:34:50 +1000

    WIP: Enemy

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,25 +6,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float acceleration = 60f;
     public float deceleration = 80f;
-    public float airAcceleration = 30f;
-    public float airDeceleration = 40f;
-
-    // Jump
-    public float jumpForce = 16f;
-    public float delayedJumpTime = 0.15f;
-    public float jumpBufferTime = 0.15f;
-    private float delayedJumpTimeCounter;
-    private float jumpBufferCounter;
-    private bool jumpHeld;
-
-    // Checks
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.25f;
-    public LayerMask groundLayer;
-    private bool isGrounded;
 
     // Input
-    private float moveInput;
+    private Vector2 moveInput;
 
     private Rigidbody2D rb;
 
@@ -35,59 +19,28 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal"); 
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        if (isGrounded)
-        {
-            delayedJumpTimeCounter = delayedJumpTime;
-        }
-        else
-        {
-            delayedJumpTimeCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jumpBufferCounter = jumpBufferTime;
-        }
-        else
-        {
-            jumpBufferCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0) 
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-
-        jumpHeld = Input.GetButton("Jump");
+        moveInput = new Vector2(moveX, moveY).normalized;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        float targetSpeed = moveInput * moveSpeed;
-        float accelRate = isGrounded ? (Mathf.Abs(targetSpeed) > 0.01f ? acceleration : deceleration)
-                                     : (Mathf.Abs(targetSpeed) > 0.01f ? airAcceleration : airDeceleration);
+        // Target velocity
+        Vector2 targetVelocity = moveInput * moveSpeed;
 
-        float speedDiff = targetSpeed - rb.linearVelocity.x; 
-        float movement = speedDiff * accelRate * Time.fixedDeltaTime;
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x + movement, rb.linearVelocity.y); 
+        // Current velocity
+        Vector2 currentVelocity = rb.linearVelocity;
 
-        if (jumpBufferCounter > 0 && delayedJumpTimeCounter > 0)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); 
-            jumpBufferCounter = 0;
-            delayedJumpTimeCounter = 0;
-        }
-    }
+        // Calculate acceleration per axis
+        float accelX = Mathf.Abs(targetVelocity.x) > 0.01f ? acceleration : deceleration;
+        float accelY = Mathf.Abs(targetVelocity.y) > 0.01f ? acceleration : deceleration;
 
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        }
+        // Apply acceleration
+        float newVelX = Mathf.MoveTowards(currentVelocity.x, targetVelocity.x, accelX * Time.fixedDeltaTime);
+        float newVelY = Mathf.MoveTowards(currentVelocity.y, targetVelocity.y, accelY * Time.fixedDeltaTime);
+
+        rb.linearVelocity = new Vector2(newVelX, newVelY);
     }
 }
